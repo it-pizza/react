66bb18f820031c93383a833651138b37cd3f0062
Артемий <fe77.dev@gmail.com>
2024-03-29 17:51:06 +0300

    feat(main.tsx): component Burger

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import "./index.css" 
 
 export function Title() {
   return <h1>Sign In</h1>
 }
 
+export function Burger() {
+  return (
+    <nav className="menu">
+      <input type="checkbox" id="burger-checkbox" className="burger-checkbox" />
+      <label htmlFor="burger-checkbox" className="burger">
+        <span className='burger-span'></span>
+      </label>
+    </nav>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Title/>
+    <Burger/>
   </React.StrictMode>,
 )
