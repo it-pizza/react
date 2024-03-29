041401ce0d59ff0c4bb4988173dea767e69d2302
Артемий <fe77.dev@gmail.com>
2024-03-29 17:48:12 +0300

    feat(main.tsx): conmponent  Title

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+export function Title() {
+  return <h1>Sign In</h1>
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Title/>
   </React.StrictMode>,
 )
