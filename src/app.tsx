d53d639033b1d9a10a8c276ec9caefa325a4f3ab
Артемий <fe77.dev@gmail.com>
2024-04-27 16:54:53 +0300

    feat(app.tsx): Post component

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,18 @@
+interface IPostData {
+    id: number
+    size: "large" | "medium" | "small"
+}
+
+function Post(props: IPostData) {
+    if (props.size === "large") {
+        return 
+    } else if (props.size === "medium") {
+        return 
+    } else {
+        return
+    }
+}
+
 export function App() {
     return (<></>)
 }
