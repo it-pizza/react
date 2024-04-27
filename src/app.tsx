6f33ea6712593c95fd662345f725d1227217594e
Артемий <fe77.dev@gmail.com>
2024-04-27 23:54:12 +0300

    feat(app.tsx): fetch image

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,12 @@
-function PostImage() {
+import { useEffect, useState } from "react"
+
+interface IPostImage {
+    image: string
+}
+
+function PostImage(props: IPostImage) {
     return (
-        <img src="#" className="post__img"></img>
+        <img src={props.image} className="post__img"></img>
     )
 }
 
@@ -53,34 +59,58 @@ function PostControlPanel() {
     )
 }
 
-function LargePost() {
+interface IPostAttribute {
+    data: IFetchPost
+}
+
+function LargePost(props: IPostAttribute) {
+    if (!props.data) { return null }
     return (
         <div className="post large">
-            <PostImage/>
+            <PostImage image={props.data.image}/>
             <PostContent/>
             <PostControlPanel/>
         </div>
     )
 }
 
-function MediumPost() {
+function MediumPost(props: IPostAttribute) {
+    if (!props.data) { return null }
     return (
         <div className="post medium">
-            <PostImage/>
+            <PostImage image={props.data.image}/>
             <PostContent/>
             <PostControlPanel/>
         </div>
     )
 }
 
-function SmallPost() {
+function SmallPost(props: IPostAttribute) {
+    if (!props.data) { return null }
     return (
         <div className="post small">
-            <PostImage/>
+            <PostImage image={props.data.image}/>
             <PostContent/>
             <PostControlPanel/>
         </div>
     )
+}
+
+interface IFetchPost {
+    "id": number
+    "image": string
+    "text": string
+    "date": string
+    "lesson_num": number
+    "title": string
+    "author": number
+}
+
+function fetchPost(id: string | number): Promise<IFetchPost> {
+    const url = "https://studapi.teachmeskills.by"
+    return fetch(`${url}/blog/posts/${id}/`)
+        .then(res => res.json())
+        .catch(error => console.error(error))
 }
 
 interface IPostData {
@@ -89,15 +119,21 @@ interface IPostData {
 }
 
 function Post(props: IPostData) {
+    const [usePostData, setUsePostData] = useState<IFetchPost>()
+
+    useEffect(() => {
+        fetchPost(props.id).then(data => setUsePostData(data))
+    }, [])
+
     if (props.size === "large") {
-        return <LargePost/>
+        return <LargePost data={usePostData!}/>
     } else if (props.size === "medium") {
-        return <MediumPost/>
+        return <MediumPost data={usePostData!}/>
     } else {
-        return <SmallPost/>
+        return <SmallPost data={usePostData!}/>
     }
 }
 
 export function App() {
-    return (<></>)
+    return (<Post size="large" id={10}/>)
 }
