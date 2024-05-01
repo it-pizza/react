1a4d9ab7db88f978c16c25f1a9fa4dbee51494f0
Артемий <fe77.dev@gmail.com>
2024-05-01 13:31:55 +0300

    feat(app.tsx): Form component

--- src/app.tsx
+++ src/app.tsx
@@ -15,11 +15,30 @@ function Header() {
     )
 }
 
+function Form() {
+    return (
+        <form method="get" className="signIn__form">
+            <div className="signIn__form-input">
+                <label htmlFor="signIn__form-email" className="signIn__form-label">Email:</label>
+                <input type="email" id="signIn__form-email" name="email" placeholder="Email" />
+            </div>
+            <div className="signIn__form-input">
+                <label htmlFor="signIn__form-password" className="signIn__form-label">Password:</label>
+                <input type="password" id="signIn__form-password" name="password" placeholder="Password" />
+            </div>
+            <a href="#">Forgot password?</a>
+            <button type="submit" className="signIn__form-button">Sign in</button>
+            <p className="signIn__create-account">Don't have as account? <a href="#">Sign Up</a></p>
+        </form>
+    )
+}
+
 function Main() {
     return (
         <main>
             <section className="signIn">
-                <h1>sign in</h1>
+                <h1 className="signIn__title">sign in</h1>
+                <Form />
             </section>
         </main>
     )
