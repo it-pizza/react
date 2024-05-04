2053f7ffcc5859d8647b8617dc89a68fcf59195c
Артемий <fe77.dev@gmail.com>
2024-05-04 22:08:49 +0300

    feat(posts.tsx): psots section

--- src/pages/posts/post.tsx
+++ src/pages/posts/post.tsx
@@ -3,7 +3,12 @@ import { Header } from "../../widgets/index.ts";
 export function PostsPage() {
     return (
         <>
-            <Header />
+            <Header /> {/* Я только в конце понял что не правильно собрал header */}
+            <main>
+                <section className="posts">
+                    
+                </section>
+            </main>
         </>
     )
 }
